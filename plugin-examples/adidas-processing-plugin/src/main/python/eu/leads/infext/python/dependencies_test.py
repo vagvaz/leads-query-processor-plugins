06529fa233c02be84725611d2caf9f58cdfbbd7d
pskorupinski <pawel.skorupinski@gmail.com>
2014-11-10 16:28:40 +0100

    python test v1

---
+++ plugin-examples/adidas-processing-plugin/src/main/python/eu/leads/infext/python/dependencies_test.py
@@ -0,0 +1,22 @@
+'''
+Created on Jan 30, 2014
+
+@author: amo remix
+'''
+
+from lxml import etree
+from sklearn import linear_model
+import requests
+
+xml = '<a xmlns="test"><b xmlns="test"/></a>'
+root = etree.fromstring(xml)
+print etree.tostring(root)
+
+
+clf = linear_model.Ridge (alpha = .5)
+print clf.fit ([[0, 0], [0, 0], [1, 1]], [0, .1, 1])
+print clf.coef_
+print clf.intercept_ 
+
+r = requests.get('https://api.github.com/events')
+print r.url
